61e98fd91784b7eb0c026550dc8751867a6300e8
joysouza.contato@gmail.com <joysouza.contato@gmail.com>
2023-06-16 21:38:41 -0300

    Aula 04 - Sidebar e preparação do banco de dados

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
 import "./App.css";
-import Video from "./pages/video"
+import Video from "./pages/Video";
 
 function App() {
   return (
     <div className="App">
       <div className="app__videos">
-      <Video />
-      <Video />
-      <Video />
-      <Video />
+      <Video 
+      likes={312}
+      messages={548}
+      shares={121}
+      name="Eminem"
+      description= "Live from Detroit"
+      music="Eminem - Sing For The Moment"
+      url="https://video-eminem.vercel.app/download.mp4"
+      />
+      <Video 
+       likes={212}
+       messages={125}
+       shares={121}
+       name="Snoop Dogg"
+       description= "Eminem and Snoop Dogg"
+       music="Eminem & Snoop Dogg - From The D 2 The LBC"
+       url="https://video-eminem.vercel.app/download_01.mp4"
+      />
+      <Video 
+       likes={321}
+       messages={352}
+       shares={121}
+       name="NFL"
+       description= "Dr. Dre, Snoop Dogg, Eminem, Mary J. Blige, Kendrick Lamar & 50 Cent FULL Pepsi SB LVI Halftime Show"
+       music="Dr. Dre - Still D.R.E. ft. Snoop Dogg"
+       url="https://video-eminem.vercel.app/download_02.mp4"
+      />
       </div>
     </div>
   );
--- src/pages/video.js
+++ src/pages/video.js
@@ -1,8 +1,9 @@
 import React, { useRef, useState } from "react";
 import VideoFooter from "./components/footer/VideoFooter";
+import VideoSidebar from "./components/sidebar/VideoSidebar";
 import "./video.css";
 
-function Video() {
+function Video({likes, messages, shares, name, description, music, url}) {
   const videoRef = useRef(null);
   const  [play, setPlay]  = useState(false);
 
@@ -24,10 +25,18 @@ function Video() {
         ref={videoRef}
         onClick={handdleStart}
         loop
-        src="https://video-eminem.vercel.app/download.mp4"
+        src={url}
       ></video>
-      {/*Side Bar*/}
-     <VideoFooter />
+      <VideoSidebar 
+      likes={likes}
+      messages={messages}
+      shares={shares}
+      />
+     <VideoFooter 
+     name={name}
+     description= {description}
+     music={music}
+     />
     </div>
   );
 }
